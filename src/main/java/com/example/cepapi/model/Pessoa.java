5f436b6b85ba768e8baeee3596042614b2251f33
camilabarpp <milabarpp5@gmail.cm>
2022-07-26 14:06:41 -0300

    Fiz algumas alterações e refiz o @Autowired
    # Conflicts:
    #	src/main/java/com/example/cepapi/controller/CepRestController.java
    #	src/main/java/com/example/cepapi/service/CadastroServices.java

--- src/main/java/com/example/cepapi/model/Pessoa.java
+++ src/main/java/com/example/cepapi/model/Pessoa.java
@@ -10,8 +10,7 @@ import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
+
 
 
 @JsonIgnoreProperties(ignoreUnknown = true)
